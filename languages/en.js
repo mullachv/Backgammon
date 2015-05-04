1c4fba4593261674ff20c58895902f9dafd7f155
Islam Tawfik <ibtawfik@gmail.com>
2015-05-03 21:23:31 -0400

    Carosel

---
+++ languages/en.js
@@ -0,0 +1,10 @@
+window.angularTranslationsLoaded('en',{
+"RULES_OF_BACKGAMMON":"Rules of BackGammon",
+"RULES_SLIDE1":"The goal of the game is to move your pieces across and off the board. The white players path is from left to right on the top board. Then left to right on the bottom board until exiting the board.",
+"RULES_SLIDE2":"The number of spaces you can move is given by a dice roll. The numbers can be split into one move or two seprate moves.",
+"RULES_SLIDE3":"Rolling two dice with the same number is a special case and allows you to move four times. This can be split up into any number of moves for a single dice",
+"RULES_SLIDE4": "Moving into a space requires that it be open. An open space is one that your opponent has less than 2 pieces on.",
+"RULES_SLIDE5": "If an opposing player has a single piece in a spot and you move to it. Then you can capture their piece and send them to the start of the board.",
+"RULES_SLIDE6": "The goal of the game is to get all your pieces off the board. First player to remove all their pieces wins",
+"CLOSE":"Close"
+});
